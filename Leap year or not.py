09d2eb8ef2c12a4afa32179b57b2da1df23e899c
liya-6607 <192519074.simats@saveetha.com>
2025-10-29 11:00:10 +0530

    Add files via upload

---
+++ Leap year or not.py
@@ -0,0 +1,6 @@
+#Leap year or not
+n=int(input('enter any number'))
+if(n%4==0):
+    print('The given year comes under the leap year')
+else:
+    print('The given year does not comes under the leap year')
